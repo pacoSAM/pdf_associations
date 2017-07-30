c7633a14d90bd4dcfd1157ab9a8da4a3b67057a7
paco sam <sambapaco@yahoo.fr>
2017-07-30 17:24:55 +0200

    PEP8 modification

--- main.py
+++ main.py
@@ -8,48 +8,50 @@ from helper_popup_window import *
 
 import os
 
+
 class MyPdfWidget(BoxLayout):
-    def __init__(self,**kwargs):
-        super(MyPdfWidget,self).__init__(**kwargs)
+
+    def __init__(self, **kwargs):
+        super(MyPdfWidget, self).__init__(**kwargs)
         self.filename = ""
         self.popwindow = ""
 
     def open(self, path, filename):
         """open button"""
         self.filename = filename
-        if len(self.filename) > 0: 
+        if len(self.filename) > 0:
             if check_format(self.filename) == False:
                 error_popup()
             else:
                 content = TextInput(
-                        pos_hint={'down': 1},
-                        size_hint=(1,0.30),
-                        multiline=False,
-                        )
+                    pos_hint={'down': 1},
+                    size_hint=(1, 0.30),
+                    multiline=False,
+                )
                 self.popwindow = Popup(title='Named your file',
-                            content=content, 
-                            size_hint=(None, None), 
-                            size=(400, 400),)
+                                       content=content,
+                                       size_hint=(None, None),
+                                       size=(400, 400),)
                 content.bind(on_text_validate=self.MainPopup_combine)
                 self.popwindow.open()
 
-    def image_to_pdf(self,path,filename):
+    def image_to_pdf(self, path, filename):
         nfile_in_repo = len(os.listdir(os.getcwd()))
         self.filename = filename
-        if len(self.filename) > 0: 
+        if len(self.filename) > 0:
             if check_format(self.filename) == False:
                 error_popup()
             else:
                 for file in self.filename:
-                    convert_image_to_pdf(file,"pdf")
+                    convert_image_to_pdf(file, "pdf")
 
                 if len(os.listdir(os.getcwd())) > nfile_in_repo:
                     ok_popup("your file ")
 
-    def MainPopup_combine(self,instance):
+    def MainPopup_combine(self, instance):
         """Text input and main for combinaison"""
-        outfilename = os.path.splitext(instance.text)[0] +".pdf"
-        combine_in_one_pdf(self.filename,outfilename)
+        outfilename = os.path.splitext(instance.text)[0] + ".pdf"
+        combine_in_one_pdf(self.filename, outfilename)
         self.popwindow.dismiss()
         ok_popup(outfilename)
 
@@ -58,9 +60,10 @@ class MyPdfWidget(BoxLayout):
 
 
 class MyPdfApp(App):
+
     def build(self):
-	self.title = 'PDF COMBINE'
-	self.icon = 'PDF.png'
+        self.title = 'PDF COMBINE'
+        self.icon = 'PDF.png'
         return MyPdfWidget()
 
 if __name__ == '__main__':
